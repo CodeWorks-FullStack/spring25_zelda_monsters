6e6af4f4712e6f36eb0777fdb7035602387dd3ff
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-03 16:25:51 -0700

    notes

--- app/controllers/MonstersController.js
+++ app/controllers/MonstersController.js
@@ -7,9 +7,13 @@ export class MonstersController {
     console.log('Monsters Controller is ready!');
     AppState.on('monsters', this.drawMonsters)
     this.getMonsters()
+
+    // NOTE since our data is coming from an API now, there will be nothing to draw from our AppState on page load
+    // ❌ this.drawMonsters()
   }
 
   getResultBack() {
+    // NOTE this bit of code will take 3 seconds for our Promise to resolve/reject
     const result = new Promise((resolve, reject) => {
       setTimeout(() => {
         const randomNum = Math.round(Math.random() * 10)
@@ -17,7 +21,6 @@ export class MonstersController {
         if (randomNum > 5) {
           resolve(`randomNum was ${randomNum}`)
         }
-
         else {
           reject(`randomNum was ${randomNum}`)
         }
@@ -32,21 +35,23 @@ export class MonstersController {
       const something = await this.getResultBack()
       console.log('what is something', something);
     } catch (error) {
-      console.error('PROMISE WAS REJECTED');
+      console.error('PROMISE WAS REJECTED', error);
     }
   }
 
+  // NOTE async modifier allows us to use the 'await' keyword within functions so that we can pause our code while a promise is resolved 
   async getMonsters() {
+    // NOTE if any error is THROWN in the try block, it catches the error and runs the catch code block instead
     try {
       await monstersService.getMonsters()
       Pop.success('Successful request to get the monsters')
-      // NOTE if any error is THROWN in the try block, it catches the error and runs this code block instead
     } catch (error) {
       console.error('Could not get monsters from API', error)
       Pop.error(error, "Could not get monsters!")
     }
   }
 
+  // NOTE nothing has changed here
   drawMonsters() {
     const monsters = AppState.monsters
     let monsterCardsContent = ''
--- app/services/MonstersService.js
+++ app/services/MonstersService.js
@@ -1,6 +1,7 @@
 import { AppState } from "../AppState.js";
 import { Monster } from "../models/Monster.js";
 
+// NOTE our axios instance that we will use to send requests to APIs
 // @ts-ignore
 const monstersApi = axios.create({
   // NOTE where we are sending our requests to
@@ -11,13 +12,13 @@ const monstersApi = axios.create({
 
 class MonstersService {
   async getMonsters() {
-
-    // NOTE sending request to 'https://botw-compendium.herokuapp.com/api/v3/compendium' + 'category/monsters'
+    // NOTE sending GET request to 'https://botw-compendium.herokuapp.com/api/v3/compendium' + '/category/monsters'
     const response = await monstersApi.get('category/monsters')
     // NOTE response data from an axios response will be the response's body (the data we care about)
     console.log('GOT MONSTERS 📡📡📡📡', response.data);
+    // NOTE map will return a new array of different data. We convert all of the objects sent from the other API into our own personal class objects
     const monsters = response.data.data.map(pojo => new Monster(pojo))
-    AppState.monsters = monsters
+    AppState.monsters = monsters // triggers observer
     console.log('monsters!', AppState.monsters);
   }
 }
