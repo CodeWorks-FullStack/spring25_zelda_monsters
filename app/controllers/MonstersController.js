69bb9ed6c5b6e7e71c6bbc965ad11292e3af1fbb
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-03 11:34:10 -0700

    promise example

--- app/controllers/MonstersController.js
+++ app/controllers/MonstersController.js
@@ -1,6 +1,23 @@
 export class MonstersController {
   constructor() {
+    this.testPromise()
     console.log('Monsters Controller is ready!');
+  }
+
+  getResultBack() {
+    const result = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('All done, here is the result')
+      }, 3000)
+    })
+
+    return result
+  }
+
+  async testPromise() {
+    const something = await this.getResultBack()
+
+    console.log('what is something', something);
 
   }
 }
