a5a1e027ad1023a6bcf10e9cdb9fb4f27ac4f389
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-03 12:22:15 -0700

    added property to appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,9 +1,13 @@
+import { Monster } from './models/Monster.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
-
+  /**
+   * @type {Monster[]}
+   */
+  monsters = []
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
